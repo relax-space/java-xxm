8b0b0fa4317db25ad808d929a1d643e95c502d21
xiao.xinmiao <xiaoxm_001@163.com>
2020-05-05 09:16:48 +0800

    subject 2: map

--- newcomer/L1.java
+++ newcomer/L1.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class L1 {
     public static void main(final String[] args) {
-        System.out.println(No1());
+        No1();
+        No2(8);
     }
 
-    public static String No1(){
+    public static void No1(){
         List<Integer> intList = new ArrayList<Integer>();
         for (int i = 2000; i <= 3200; i++) {
             if (i %7 ==0 && i %5 !=0){
@@ -15,6 +18,14 @@ public class L1 {
             }
         }
         String joined = intList.stream().map(String::valueOf).collect(Collectors.joining(","));
-        return joined;
+        System.out.println(joined);
+    }
+    public static void No2(int input){
+        Map<Integer, Integer> result = new HashMap<Integer,Integer>();
+        for (int i = 1; i < input+1; i++) {
+            result.put(i,i*i);
+        }
+
+        System.out.println(result);
     }
 }
